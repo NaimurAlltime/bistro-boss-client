5c5b7eb6f04474b4bd80691db0b77f886a5d0789
Naimur Rahman <naimur2231@gmail.com>
2023-05-27 10:26:42 +0600

    Shared section title for different sections

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div className="md:w-3/12 mx-auto text-center">
+      <p className="text-lg font-medium text-yellow-500">{subHeading}</p>
+      <h3 className="text-3xl uppercase font-semibold border-y-4 py-2 mt-2">
+        {heading}
+      </h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -11,14 +11,15 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <div className="mb-10 mt-14">
-      <p className="text-lg font-medium text-yellow-500 text-center">
-        ---From 11:00am to 10:00pm---
-      </p>
-      <h2 className="text-3xl font-semibold text-center mt-3">ORDER ONLINE</h2>
+    <section className="mb-10 mt-14">
+      <SectionTitle
+        subHeading={"---From 11:00am to 10:00pm---"}
+        heading={"ORDER ONLINE"}
+      ></SectionTitle>
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -31,36 +32,36 @@ const Category = () => {
       >
         <SwiperSlide>
           <img src={slide1} alt="" />
-          <h3 className="text-2xl font-normal uppercase text-white text-center">
+          <h3 className="text-lg md:text-2xl font-normal uppercase text-white text-center">
             Salads
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide2} alt="" />
-          <h3 className="text-2xl font-normal uppercase -mt-16 text-white text-center">
+          <h3 className="text-lg md:text-2xl md:-mt-16 font-normal uppercase -mt-9 text-white text-center">
             Soups
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide3} alt="" />
-          <h3 className="text-2xl font-normal uppercase -mt-16 text-white text-center">
+          <h3 className="text-lg md:text-2xl md:-mt-16 font-normal uppercase -mt-9 text-white text-center">
             pizzas
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide4} alt="" />
-          <h3 className="text-2xl font-normal uppercase  -mt-16 text-white text-center">
+          <h3 className="text-lg md:text-2xl md:-mt-16 font-normal uppercase  -mt-9 text-white text-center">
             desserts
           </h3>
         </SwiperSlide>
         <SwiperSlide>
           <img src={slide5} alt="" />
-          <h3 className="text-2xl font-normal uppercase  -mt-16 text-white text-center">
+          <h3 className="text-lg md:text-2xl md:-mt-16 font-normal uppercase  -mt-9 text-white text-center">
             Salads
           </h3>
         </SwiperSlide>
       </Swiper>
-    </div>
+    </section>
   );
 };
 
